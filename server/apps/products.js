20481fd58ebcd7fcf2354f4cb81323d173a4517d
Lhing <thorntan.r@gmail.com>
2024-01-13 02:24:13 +0700

    feat: completed ex1-5

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,63 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const product = await collection.find({}).limit(10).toArray();
+    return res.json({ data: product });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.id); // id จาก endpoint param
+    // หาข้อมูล _id ที่ ต้องการ คือ productID โดยหาแค่ 1 ข้อมูล จึงใช้ findOne
+    const getProductById = await collection.findOne({ _id: productId });
+    return res.json({ data: getProductById });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  try {
+    const productData = { ...req.body }; // ข้อมูลที่มาจาก Request body
+    const collection = db.collection("products");
+    await collection.insertOne(productData); // เพิ่มข้อมูลเข้าไปใน db
+    return res.json({ message: `Product has been created successfully` });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  try {
+    const updateProduct = { ...req.body }; // ข้อมูลที่ส่งมาจาก request body
+    const collection = db.collection("products"); // เลือก database
+    const productId = new ObjectId(req.params.id); // id จาก endpoint param
+    await collection.updateOne({ _id: productId }, { $set: updateProduct });
+    return res.json({ message: "Product has been updated successfully" });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.delete("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productId = new ObjectId(req.params.id);
+    await collection.deleteOne({ _id: productId });
+    return res.json({ message: "Product has been deleted successfully" });
+  } catch (error) {
+    return res.json({ message: `${error}` });
+  }
+});
 
 export default productRouter;
